f7ea31d3ee68e70b021a8e4727acfc98ede31aa1
Denis <job.dbarsukov@gmail.com>
2020-02-07 23:24:31 +0300

    create GCE instance from "docker-base" image

--- dockermonolith/infra/ansible-play.sh
+++ dockermonolith/infra/ansible-play.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-ansible-playbook ./ansible/playbooks/docker_ubuntu1604/playbook.yml ./ansible/playbooks/reddit_container/playbook.yml
+#ansible-playbook ./ansible/playbooks/docker_ubuntu1604/playbook.yml
+ansible-playbook ./ansible/playbooks/reddit_container/playbook.yml
